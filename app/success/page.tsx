b1b908810610c74ef88f8e22a876c2362790bf72
adiii3692 <adinair0206@gmail.com>
2025-02-14 00:34:38 -0500

    Changed icon and added footer to next page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -15,6 +15,9 @@ export default function Home(){
             <div className='flex items-center justify-center mt-12'>
                 <Image src={pokemon} alt='pokemon card' className='w-[80%] h-[50%]'/>
             </div>
+            <footer className="mt-6 row-start-3 flex gap-6 flex-wrap items-center justify-center">
+                <p>Made with &hearts; by Adi (Ur Pookie fr)</p>
+            </footer>
         </div>
     )
 }
